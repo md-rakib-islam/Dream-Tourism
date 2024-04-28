8632518507b826e608b14f3e60ff23b825ccbae3
Rakibul Islam <rakib.islam0000@gmail.com>
2024-04-28 09:40:06 +0600

    last updated

--- components/tours/Tours.jsx
+++ components/tours/Tours.jsx
@@ -96,7 +96,7 @@ const Tours = ({ destination }) => {
       <div key={item?.id}>
         <div class="container">
           <div class="row">
-            <div class="col-3">
+            <div class="col-lg-3 col-md-3 col-8">
               <Link
                 href={`/tour/${item?.title
                   ?.replace(/[,.-]/g, "")
