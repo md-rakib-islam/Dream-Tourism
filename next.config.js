4cfb52bbbe9cdc7ef5abe47105f23db1e6308558
Rakibul Islam <rakib.islam0000@gmail.com>
2024-05-23 18:01:30 +0600

    some updated

--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ const imagedeliveryConfig = {
   hostname: "imagedelivery.net",
   pathname: "/",
 };
+
 const nextConfig = {
   images: {
     domains: ["imagedelivery.net"],
@@ -25,6 +26,41 @@ const nextConfig = {
     minimumCacheTTL: 60,
   },
   output: "standalone",
+
+  async redirects() {
+    return [
+      {
+        source: "/italy",
+        destination: "/destinations/italy",
+        permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+      {
+        source: "/netherlands",
+        destination: "/destinations/netherlands",
+        permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+      {
+        source: "/switzerland",
+        destination: "/destinations/switzerland",
+        permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+      {
+        source: "/germany",
+        destination: "/destinations/germany",
+        permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+      {
+        source: "/belgium",
+        destination: "/destinations/belgium",
+        permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+      {
+        source: "/france",
+        destination: "/destinations/france",
+        permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
